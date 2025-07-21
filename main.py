852c94c4042b564b1e4a174c55e91d54b80897e4
Nettosh-f <netta.bensinai@gmail.com>
2025-07-21 14:05:56 +0300

    added health check

--- main.py
+++ main.py
@@ -66,6 +66,12 @@ async def slack_events(req: Request):
 
     return {"ok": True}
 
+
+@app.get("/")
+def read_root():
+    return {"status": "running"}
+
+
 if __name__ == "__main__":
 
     uvicorn.run("main:app", host="127.0.0.1", port=3000, reload=True)
